4dcea1b7808605f04557cad0f285658633fc2b4f
Alvaro Castro Cid <58068322+fidotheprince@users.noreply.github.com>
2023-07-22 01:30:54 +0000

    added docstrings

--- app.py
+++ app.py
@@ -1,34 +1,51 @@
+"""
+This module handles task data provided by a user, processes the input, 
+saves it to a file, and analyzes the tasks to answer a preset question.
+"""
+
 from langchain.document_loaders import TextLoader
 from langchain.indexes import VectorstoreIndexCreator
 
-# a method that checks if the user has entered a number
 def is_number(num_str):
+    """
+    Check if the provided string can be converted to an integer.
+    
+    Parameters:
+    num_str (str): The string to be checked.
+    
+    Returns:
+    bool: True if the string can be converted to an integer, False otherwise.
+    """
     try:
         int(num_str)
         return True
     except ValueError:
         return False
 
-# a method that analyzes the text derived from the user
 def analyze_text():
+    """
+    Load a text document, create an index for it, and use this index 
+    to answer a pre-defined question.
+    """
     # Document loader
     loader = TextLoader("data.txt")
     # Index that wraps above steps
     index = VectorstoreIndexCreator().from_loaders([loader])
     # Question-answering
-    question = "What's the most time effective way to complete each of the tasks in the data.txt file?"
+    question = "Can you tell exercises or habit I can do to optimize my physical and cognative ability to complete each task?"
 
     answer = index.query(question)
 
     print(answer)
 
-# a method that collects the data from the user and implements the above methods
 def collect_data():
+    """
+    Collect a number of tasks from the user, save them to a text file, 
+    and call analyze_text() to process and analyze the tasks.
+    """
     # Prompt the user to enter some text
     num_str = input("Hello how many tasks do you have to complete today? ")
-    
     if is_number(num_str):
-        
         print("Thank you, please enter your tasks below:")
 
         num_times = int(num_str)
@@ -37,20 +54,21 @@ def collect_data():
 
             text = input(f"Please enter a task {i}: ")
             # Open the file in write mode ('w')
-            with open("data.txt", "a") as file:
+            with open("data.txt", "a", encoding='utf-8') as file:
                 # Write the text to the file
                 file.write(f"Task {i}: " + text + "\n")
                 # Close the connection to the file
-        
         print("Thank you, your tasks have been saved, please wait while we analyze your tasks...")
         analyze_text()
     else:
         print("You did not enter a number")
 
-
-
 def main():
+    """
+    Main entry point of the application.
+    """
     collect_data()
 
 
-main()
+if __name__ == "__main__":
+    main()
